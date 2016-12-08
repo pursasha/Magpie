0dc80a2f1bfcb29b5c8d3d36e8773afddfd00a6a
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2016-12-08 13:34:20 -0500

    space

--- src/Magpie5.java
+++ src/Magpie5.java
@@ -37,7 +37,7 @@ public class Magpie5
 		String response = "";
 		if (statement.length() == 0)
 		{
-			response = "Say something, please.";
+			response = "Say something, please. ";
 		}
 
 		else if (findKeyword(statement, "no") >= 0)
