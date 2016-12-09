185ef3f82bb286f33714e72756637bb318ef32d9
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2016-12-09 13:42:35 -0500

    memory method

--- src/Miscellaneous.java
+++ src/Miscellaneous.java
@@ -1,3 +1,10 @@
 public class Miscellaneous {
-	
+	public boolean memory(String statement, String usage) {
+		if (usage.compareTo("store") == 0) {
+			
+		}
+		else if (usage.compareTo("access") == 0) {
+			
+		}
+	}
 }
