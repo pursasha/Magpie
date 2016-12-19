a7f5c1a8b9e320c2a4e2762ec4a1c73428f3f38d
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2016-12-19 13:29:19 -0500

    and yet another change to memory method

--- src/Miscellaneous.java
+++ src/Miscellaneous.java
@@ -1,6 +1,7 @@
 public class Miscellaneous {
-	static String[] memory = new String[10]; //memo stores up to a certain amt strings before it starts rewriting data
-	static int memSlot = 0; //determines where the string is placed at the memory array
+	//memory section
+	private static String[] memory = new String[10]; //memo stores up to a certain amt strings before it starts rewriting data
+	private static int memSlot = 0; //determines where the string is placed at the memory array
 	public static boolean memory(String statement, String usage) { //memory can either store strings or access strings
 		boolean verify = false;
 		if (usage.compareTo("store") == 0) { //stores string in as part of the memory array
@@ -17,6 +18,7 @@ public class Miscellaneous {
 				}
 				else if (statement.compareTo(memory[x]) == 0) {
 					verify = true; //change [verify] to true so it'll confirm that statement has been used recently
+					break; //if true, breaks the loop and goes instantly to return so that time isnt wasted
 				}
 			}
 		}
