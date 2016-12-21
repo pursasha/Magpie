90b332b7af36feea030db0daed8abc8d470d8c41
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2016-12-21 13:34:51 -0500

    Added blacklist for memory

--- src/Miscellaneous.java
+++ src/Miscellaneous.java
@@ -2,6 +2,7 @@
 public class Miscellaneous {
 	//memory section
 	private static String[] memory = new String[10]; //memo stores up to a certain amt strings before it starts rewriting data
+	private static String[] ignoreList = {"yes", "no", "date"};
 	private static int memSlot = 0; //determines where the string is placed at the memory array
 	public static boolean memory(String statement, String usage) { //memory can either store strings or access strings
 		boolean verify = false;
@@ -17,11 +18,12 @@ public class Miscellaneous {
 				if (memory[x] == null) { //passes through null elements to avoid errors
 					continue;
 				}
-				else if (findKeyword(memory[x], "yes") >= 0
-						||findKeyword(memory[x], "no") >= 0) {
-							continue;
-						}
-				else if (statement.compareTo(memory[x]) == 0) {
+				for(int y = 0; y < ignoreList.length; y++) { //check for blacklisted words and ignores those words if they come up
+					if (findKeyword(memory[x], ignoreList[y]) >= 0) {
+						continue;
+					}
+				}
+				if (statement.compareTo(memory[x]) == 0) {
 					verify = true; //change [verify] to true so it'll confirm that statement has been used recently
 					break; //if true, breaks the loop and goes instantly to return so that time isnt wasted
 				}
