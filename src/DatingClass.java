00ee3682f11c98978cd50fef3d697c9da6f98b67
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2016-12-21 13:29:43 -0500

    comments and  changes to dating

--- src/DatingClass.java
+++ src/DatingClass.java
@@ -1,22 +1,26 @@
-//Alexandra
-public class DatingClass {
-	private static int x=0;
+//Alexandra Gostev
+//DatingClass
+//Mr.Levin BTHS CS 
+//12.20.16
+public class DatingClass 
+{
+	private static int x=0; //variable used only in this class
 
-	public static String getResponse1(String statement)
+	public static String getResponse1(String statement) //gets customized responses from user when a certain keyword is said/
 	{
-		statement= statement.toLowerCase();
+		statement= statement.toLowerCase(); // Makes sure that there are no capitlization errors
 		String response = "";
-		if (Miscellaneous.findKeyword(statement, "Date",0) >= 0)
+		if (Miscellaneous.findKeyword(statement, "Date",0) >= 0) //uses the method findkeyword in class miscellaneous.
 		{
 			response = "Do you want to go on a computer date?";
 			x=1;
 		}
-		else if (Miscellaneous.findKeyword(statement, "yes",0) >=0 && (x==1))
+		else if (Miscellaneous.findKeyword(statement, "yes",0) >=0 && (x==1))// This x==1 will make sure that this doesnt run when they arent on a date.
 		{
 			response = "Of course you do. Everyone does. Do you think I am hot ?";	
 			x=2;
 		}
-		else if(Miscellaneous.findKeyword(statement, "no",0) >=0 && (x==1))
+		else if(Miscellaneous.findKeyword(statement, "no",0) >=0 && (x==1)) // Numbers allow for the robot to have a conversation 
 		{
 			response = "I dont take no for an answer. Do you think I am hot? "; 
 			x=2;
@@ -44,12 +48,12 @@ public class DatingClass {
 		}
 		else
 		{
-			response = datepun();
+			response = datepun(); //refers to the random response list/ 
 		}
 		
 		return response;
 	}
-		
+	 // random responses used in other parts of the this class
 		static private String datepun()
 		{
 			String[] responseList = {"You know you look really good when you wear makeup",
