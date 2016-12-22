81a9d44e64ac3e40b5eeac53c0b27cdbb44154c9
JiaHua <JiaHua@Jawa_Main.fios-router.home>
2016-12-21 21:22:26 -0500

    Fixed memory again

--- src/Miscellaneous.java
+++ src/Miscellaneous.java
@@ -2,7 +2,6 @@
 public class Miscellaneous {
 	//memory section
 	private static String[] memory = new String[10]; //memo stores up to a certain amt strings before it starts rewriting data
-	private static String[] ignoreList = {"yes", "no", "date"};
 	private static int memSlot = 0; //determines where the string is placed at the memory array
 	public static boolean memory(String statement, String usage) { //memory can either store strings or access strings
 		boolean verify = false;
@@ -18,10 +17,10 @@ public class Miscellaneous {
 				if (memory[x] == null) { //passes through null elements to avoid errors
 					continue;
 				}
-				for(int y = 0; y < ignoreList.length; y++) { //check for blacklisted words and ignores those words if they appear
-					if (findKeyword(memory[x], ignoreList[y]) >= 0) {
-						continue;
-					}
+				else if (findKeyword(memory[x], "yes") >= 0 //ignores certain words so that they can be used more than once
+						||findKeyword(memory[x], "no") >= 0
+						||findKeyword(memory[x], "date") >= 0) {
+					continue;
 				}
 				if (statement.compareTo(memory[x]) == 0) {
 					verify = true; //change [verify] to true so it'll confirm that statement has been used recently
