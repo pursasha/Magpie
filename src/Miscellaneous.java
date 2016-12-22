248bbd73e1949ce3cc0eaf8f78bf3e8cb9cb3a39
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2016-12-22 13:16:34 -0500

    gjih

--- src/Miscellaneous.java
+++ src/Miscellaneous.java
@@ -1,7 +1,7 @@
 //Jia Hua Zhu, AP Comp Sci Java, Miscellaneous tools/ methods library
 public class Miscellaneous {
 	//memory section
-	private static String[] memory = new String[10]; //memo stores up to a certain amt strings before it starts rewriting data
+	private static String[] memory = new String[3]; //memo stores up to a certain amt strings before it starts rewriting data
 	private static int memSlot = 0; //determines where the string is placed at the memory array
 	public static boolean memory(String statement, String usage) { //memory can either store strings or access strings
 		boolean verify = false;
